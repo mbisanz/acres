f640e2b65914b4ac14d844fb9c0963d969e1856f
Martin Bisanz <martin.bisanz@prodyna.de>
2014-04-11 15:12:13 +0200

    simplified common project

---
+++ acres-service-common/src/main/java/com/prodyna/pac/acres/common/cdi/EntityManagerProducer.java
@@ -0,0 +1,18 @@
+package com.prodyna.pac.acres.common.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Produces
+	public EntityManager produceEntityManager() {
+		return em;
+	}
+}
