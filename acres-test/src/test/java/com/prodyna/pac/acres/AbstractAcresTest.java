7f0e6423d0754a8317d1eec2f173908eccf06fb6
mbisanz <martin.bisanz@prodyna.de>
2014-06-03 17:09:02 +0200

    test project

--- acres-test/src/test/java/com/prodyna/pac/acres/AbstractAcresTest.java
+++ acres-test/src/test/java/com/prodyna/pac/acres/AbstractAcresTest.java
@@ -1,5 +1,18 @@
 package com.prodyna.pac.acres;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
 public abstract class AbstractAcresTest {
 
+	@Deployment
+	public static WebArchive createDeployment() {
+		WebArchive wa = ShrinkWrap.create(WebArchive.class, "test.war");
+		wa.addPackages(true, "com.prodyna.pac.acres");
+		wa.addAsResource("META-INF/persistence.xml");
+		wa.addAsWebInfResource("beans.xml");
+		System.out.println(wa.toString(true));
+		return wa;
+	}
 }
